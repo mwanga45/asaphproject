a5ef6bb75cc27a58936468f8986d9d8397ec585d
Lynxprazo <issamwanga02@gmail.com>
2025-05-12 19:19:19 +0300

    more changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,21 @@
-import React, { useEffect } from 'react'
-import "./landing.css"
-import 
+'use client'
+
+import React, { useEffect } from "react";
+import "./landing.css";
 
 export default function Landingpage() {
-  useEffect(()=>{
-    setInterval(()=>({
-      
-    }))
-  },[])
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      window.location.href='./authentic/login'
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className='main-container'>
-      <h1>Welcome to our Name of Site </h1>
+    <div className="main-container">
+      <h1>Welcome to our Name of Site</h1>
     </div>
-  )
+  );
 }
