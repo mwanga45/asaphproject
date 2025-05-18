f67582b4234ecce68230f77519598aeb1ba432c0
Lynxprazo <issamwanga02@gmail.com>
2025-05-18 13:32:10 +0300

    navigation bar

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,10 +1,12 @@
-'use client '
+"use client"
 import React from 'react'
+import Navbar from "../componets/navigation"
 import './home.css'
 
 function Home() {
   return (
     <div className='hm-container'>
+        <Navbar/>
         <div></div>
         <div></div>
         <div></div>
