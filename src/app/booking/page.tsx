46a43e99cad251ea5823eb9480bd26cfe62b34e0
Lynxprazo <issamwanga02@gmail.com>
2025-05-21 16:33:19 +0300

    booking changes

--- src/app/booking/page.tsx
+++ src/app/booking/page.tsx
@@ -1,43 +1,40 @@
-'use client'
-import React from 'react'
-// import Navbar from '../componets/navigation'
+"use client";
+import React from "react";
+import Navbar from "../componets/navigation";
 // import Link from 'next/link'
-import './booking.css'
+import "./booking.css";
 function Booking() {
   const Dropdown = [
-    {id: 1 , servicename: 'General Check'},
-    {id: 2 , servicename: 'Malaria'},
-    {id: 3, servicename: 'Eye Clinic'},
-    {id: 4 , servicename: 'Skin Clinic'}
-    
-  ]
+    { id: 1, servicename: "General Check" },
+    { id: 2, servicename: "Malaria" },
+    { id: 3, servicename: "Eye Clinic" },
+    { id: 4, servicename: "Skin Clinic" },
+  ];
   return (
-    <div>
-      <div className='nav-container'>
-         {/* <Navbar/> */}
-         <div className="bk-title">
-          <p className='title-content'> Welcome to Our Booking </p> 
-         </div>
-         <div className="ServiceListContainer">
-              <form >
-                <select name="" id="" className='service-ls'>
-                <option value="">--Select Service--</option>
-                {Dropdown.map((service)=>(
-                    <option value="id" key={service.id}>{service.servicename}</option>
-                ))}
-                </select>
-                <button className='bk-btn'>Get Slot!</button>
-              </form>
-         </div>
-         <div className="resultsheet">
-
-         </div>
-         <div className="bookingsheetcointainer">
-            
-         </div>
+    <div className="main-container">
+      <Navbar />
+      <div className="nav-container">
+        <div className="bk-title">
+          <p className="title-content"> Welcome to Our Booking </p>
+        </div>
+        <div className="ServiceListContainer">
+          <form>
+            <select name="" id="" className="service-ls">
+              <option value="">--Select Service--</option>
+              {Dropdown.map((service) => (
+                <option value="id" key={service.id}>
+                  {service.servicename}
+                </option>
+              ))}
+            </select>
+            <button className="bk-btn">Get Slot!</button>
+          </form>
+        </div>
+        <div className="resultsheet"></div>
+        <div className="bookingsheetcointainer"></div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Booking
+export default Booking;
