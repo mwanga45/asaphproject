6b60dfbbf175c565682b61f9116da288e8e1697d
Lynxprazo <issamwanga02@gmail.com>
2025-05-26 06:54:18 +0300

    more changes

--- src/app/booking/page.tsx
+++ src/app/booking/page.tsx
@@ -1,53 +1,56 @@
 "use client";
-import React,{useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../componets/navigation";
 import { apiURL } from "../utils/Urlport";
-import { ToastContainer,toast } from "react-toastify";
+import { ToastContainer, toast } from "react-toastify";
 import "./booking.css";
 import axios from "axios";
 type service = {
-  id: number,
-  servicename:string,
-  duration_minutes:string
-  fee:string
-}
-const  Booking:React.FC =()=> {
-  const [services, setservices]= useState<service[]>([])
-  // const handleGetslot = async()=>{
-
-  // }
-  const handleService = async()=>{
-    try{
-      const res =  await axios.get(apiURL+"api/service/profile/seviceAvailable")
-      switch(res.status){
+  id: number;
+  servicename: string;
+  duration_minutes: string;
+  fee: string;
+};
+const Booking: React.FC = () => {
+  const [services, setservices] = useState<service[]>([]);
+  const handleService = async () => {
+    try {
+      const res = await axios.get(
+        apiURL+"api/service/profile/seviceAvailable"
+      );
+      switch (res.status) {
         case 200:
-          toast.success(res.data.message)
-          const serv :service[] =res.data.rows
-          setservices(serv)
-          break
+          toast.success(res.data.message);
+          const serv: service[] = res.data.rows;
+          setservices(serv);
+          break;
         case 400:
-          toast.error("Something went wrong or Token is been Expire")
+          toast.error("Something went wrong or Token is been Expire");
+          break;
+        case 404:
+          toast.error(res.data.message)
           break
         case 500:
-          toast.error("Internal Server Error ")
-          break
-          default:
-            toast.error("Unexpected Status"+ res.status)
+          toast.error("Internal Server Error ");
+          break;
+        default:
+          toast.error("Unexpected Status" + res.status);
+          break;
       }
-    }catch(err){
-      console.error("Something went wrong", err)
-      return  alert("Internal Server Error")
+    } catch (err) {
+      console.error("Something went wrong", err);
+      return alert("Internal Server Error");
     }
-  }
+  };
   useEffect(() => {
     handleService();
-  },[]);
+  }, []);
 
   return (
     <div className="mainbk-container">
       <Navbar />
       <div className="bk-container">
-        <ToastContainer/>
+        <ToastContainer />
         <div className="bk-title">
           <p className="title-content"> Welcome to Our Booking </p>
         </div>
@@ -55,7 +58,7 @@ const  Booking:React.FC =()=> {
           <form>
             <select name="service" id="service" className="service-ls">
               <option value="">--Select Service--</option>
-                   {services.map((service) => (
+              {services.map((service) => (
                 <option value={service.id} key={service.id}>
                   {service.servicename}
                 </option>
@@ -69,6 +72,6 @@ const  Booking:React.FC =()=> {
       </div>
     </div>
   );
-}
+};
 
 export default Booking;
--- src/app/utils/Urlport.tsx
+++ src/app/utils/Urlport.tsx
@@ -1,2 +1,3 @@
 // export const apiURL =  "http://192.168.137.1:8200/"
-export const apiURL =  "http://192.168.154.251:8200/"
+// export const apiURL =  "http://192.168.154.251:8200/"
+export const apiURL =  "http://localhost:8200/"
